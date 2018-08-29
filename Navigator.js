a80e915ffbff53b45e1ec471a7334f2812cde2a5
Carlton Joseph <carlton.joseph@gmail.com>
2018-08-28 21:15:25 -0700

    first commit

---
+++ Navigator.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Animated, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
+
+export const Route = () => null;
+
+const buildSceneConfig = (children = []) => {
+  const config = {};
+
+  children.forEach(child => {
+    config[child.props.name] = {
+      key: child.props.name,
+      component: child.props.component
+    };
+  });
+
+  return config;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  scene: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1
+  }
+});
+export class Navigator extends Component {
+  constructor(props) {
+    super(props);
+    const sceneConfig = buildSceneConfig(props.children);
+    const initialSceneName = props.children[0].props.name;
+    this.state = { sceneConfig, stack: [sceneConfig[initialSceneName]] };
+  }
+  _animatedValue = new Animated.Value(0);
+  handlePush = sceneName =>
+    this.setState(
+      state => ({
+        stack: [...state.stack, state.sceneConfig[sceneName]]
+      }),
+      () => {
+        this._animatedValue.setValue(width);
+        Animated.timing(this._animatedValue, {
+          toValue: 0,
+          duration: 250,
+          useNativeDriver: true
+        }).start();
+      }
+    );
+  handlePop = () =>
+    Animated.timing(this._animatedValue, {
+      toValue: width,
+      duration: 250,
+      useNativeDriver: true
+    }).start(() => {
+      // then end animation value is use after the present screen is pop'ed
+      // of the stack below which is off screen so zero it below.
+      this._animatedValue.setValue(0);
+      this.setState(({ stack }) => {
+        return {
+          stack: stack.length > 1 ? stack.slice(0, stack.length - 1) : stack
+        };
+      });
+    });
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.stack.map(({ component: CurrentScene, key }, index) => (
+          <Animated.View
+            style={
+              index === this.state.stack.length - 1 && index > 0
+                ? [
+                    styles.scene,
+                    {
+                      transform: [{ translateX: this._animatedValue }]
+                    }
+                  ]
+                : [styles.scene]
+            }
+            key={key}
+          >
+            <CurrentScene
+              navigator={{ push: this.handlePush, pop: this.handlePop }}
+            />
+          </Animated.View>
+        ))}
+      </View>
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native Navigation
+
+Construct a navigation/router from the ground up base on the code from the
+[Build A JavaScript Navigator for React Native](https://www.fullstackreact.com/articles/build-your-own-javascript-navigator-for-react-native/)
+article.
